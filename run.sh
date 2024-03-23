b3415b5b2cc10b1d96121808c1ef77c897519e13
Siegrain <100915706+Denny0x@users.noreply.github.com>
2024-03-23 16:58:11 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-f64c2996-32b7-4ec5-b0fd-87a59e5d536b//)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-f64c2996-32b7-4ec5-b0fd-87a59e5d536b/)
 
 
 echo "$curl_output" >> Update.tsx
